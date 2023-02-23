c0b748b45a5b55c07e46bd77eed958712d8d4c8f
sungyoung1107 <dong09161107@gmail.com>
2023-02-23 09:39:13 +0900

    second

---
+++ day02/src/day02/P55.java
@@ -0,0 +1,16 @@
+package day02;
+
+public class P55 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 2000000000;
+		int b = 2000000000;
+		long c = a;
+		long d = b;
+		
+		System.out.println(c+d);
+		
+	}
+
+}
