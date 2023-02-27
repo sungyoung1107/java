896b9775bfcd0d82a1ae2f82c5dc20195d154d57
sungyoung1107 <dong09161107@gmail.com>
2023-02-27 09:38:13 +0900

    gg

--- day03/src/whilecontrol/P99.java
+++ day03/src/whilecontrol/P99.java
@@ -4,9 +4,10 @@ public class P99 {
 
 	public static void main(String[] args) {
 		
-		int bb = 200;		
-		int a =10;		
-		int b = 20;
+		int bb = 200;
+		
+		int a =10;
+		int aa = 100;
 		
 		for(int i=2;i<10;i++) {
 			System.out.printf("%d 단 \n",i);
