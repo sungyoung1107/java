c304d32e764752df65ded369cf2e78ff3f16b99c
sungyoung1107 <dong09161107@gmail.com>
2023-02-27 10:13:39 +0900

    0227

--- day03/src/whilecontrol/P99.java
+++ day03/src/whilecontrol/P99.java
@@ -11,10 +11,13 @@ public class P99 {
 		int d = 10;
 		int cc = 100;
 		
+		myfor:
 		for(int i=2;i<10;i++) {
 			System.out.printf("%d 단 \n",i);
 			for(int j=1;j<10;j++) {
 				System.out.printf("%d * %d = %d \n",i,j,(i*j));
+				if (i*j==21)
+					break myfor;
 			}
 		}
 	}
