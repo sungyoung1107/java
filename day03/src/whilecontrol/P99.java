5e9de12b5d3f04fcb297639767059c3e3b84c139
sungyoung1107 <dong09161107@gmail.com>
2023-02-27 09:24:32 +0900

    commit

--- day03/src/whilecontrol/P99.java
+++ day03/src/whilecontrol/P99.java
@@ -3,6 +3,9 @@ package whilecontrol;
 public class P99 {
 
 	public static void main(String[] args) {
+		
+		int bb = 200;
+		
 		for(int i=2;i<10;i++) {
 			System.out.printf("%d 단 \n",i);
 			for(int j=1;j<10;j++) {
